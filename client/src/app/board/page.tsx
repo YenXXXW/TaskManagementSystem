fdde908dc01551e3c8bb65760ef5a23187f2e060
Wai <wai.yan@impact-h.co.jp>
2025-05-08 19:13:31 +0630

    buld erro fix

--- client/src/app/board/page.tsx
+++ client/src/app/board/page.tsx
@@ -1,26 +1,24 @@
-import { redirect } from 'next/navigation';
-import { cookies } from 'next/headers';
-import BoardToggle from "./BoardToggle";
-import ShowByStatusPage from "./showByStatus";
-import ShowByPriorityPage from "./showByPriority";
+'use client'
+
+import { useRouter } from 'next/navigation';
+import BoardToggle from './BoardToggle';
+import ShowByStatusPage from './showByStatus';
+import ShowByPriorityPage from './showByPriority';
+import { useAppSelector } from '@/state/hooks';
 
 export default function BoardPage({ searchParams }: { searchParams: { view?: string } }) {
   const view = searchParams.view || 'status';
-  const cookieStore = cookies();
-  const token = cookieStore.get('token')?.value;
+  const router = useRouter();
+  const token = useAppSelector(state => state.user.token)
 
-  if (!token) {
-    redirect('/auth/login');
+  if (token === '') {
+    router.push('/auth/login');
   }
 
   return (
     <div>
       <BoardToggle />
-      {view === 'priority' ? (
-        <ShowByPriorityPage />
-      ) : (
-        <ShowByStatusPage />
-      )}
+      {view === 'priority' ? <ShowByPriorityPage /> : <ShowByStatusPage />}
     </div>
   );
 }
