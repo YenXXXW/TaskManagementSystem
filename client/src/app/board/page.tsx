457e680ffa74f0dd8585db2ef771438facc8b9b4
Wai <wai.yan@impact-h.co.jp>
2025-05-08 19:01:08 +0630

    fix build erro

--- client/src/app/board/page.tsx
+++ client/src/app/board/page.tsx
@@ -4,26 +4,23 @@ import BoardToggle from "./BoardToggle";
 import ShowByStatusPage from "./showByStatus";
 import ShowByPriorityPage from "./showByPriority";
 
-export default async function BoardPage({ searchParams }: { searchParams: { view?: string } }) {
+export default function BoardPage({ searchParams }: { searchParams: { view?: string } }) {
   const view = searchParams.view || 'status';
-  const cookieStore = await cookies();
+  const cookieStore = cookies();
   const token = cookieStore.get('token')?.value;
+
   if (!token) {
-
-    redirect('/auth/login')
+    redirect('/auth/login');
   }
-
-
 
   return (
     <div>
       <BoardToggle />
-
       {view === 'priority' ? (
         <ShowByPriorityPage />
       ) : (
         <ShowByStatusPage />
       )}
     </div>
-  )
+  );
 }
